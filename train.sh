01b432f01e0a61c452c5648782f9c9fbbdb33511
Agam Goyal <goyalagam0@gmail.com>
2023-12-25 08:46:01 -0600

    add training script

---
+++ train.sh
@@ -0,0 +1,19 @@
+python3 scripts/oscillator_datagen.py -m FCA -n NWS -k 25 
+python3 scripts/oscillator_datagen.py -m FCA -n ER -k 25 
+python3 scripts/oscillator_datagen.py -m FCA -n BA -k 25 
+python3 scripts/oscillator_datagen.py -m FCA -n NWS -k 35 
+python3 scripts/oscillator_datagen.py -m FCA -n ER -k 35 
+python3 scripts/oscillator_datagen.py -m FCA -n BA -k 35 
+python3 scripts/oscillator_datagen.py -m FCA -n NWS -k 50
+python3 scripts/oscillator_datagen.py -m FCA -n ER -k 50 
+python3 scripts/oscillator_datagen.py -m FCA -n BA -k 50 
+
+python3 main.py -m FCA -n NWS -k 25 -r 4 8 12 16
+python3 main.py -m FCA -n ER -k 25 -r 4 8 12 16
+python3 main.py -m FCA -n BA -k 25 -r 4 8 12 16
+python3 main.py -m FCA -n NWS -k 35 -r 4 8 12 16
+python3 main.py -m FCA -n ER -k 35 -r 4 8 12 16
+python3 main.py -m FCA -n BA -k 35 -r 4 8 12 16
+python3 main.py -m FCA -n NWS -k 50 -r 4 8 12 16
+python3 main.py -m FCA -n ER -k 50 -r 4 8 12 16
+python3 main.py -m FCA -n BA -k 50 -r 4 8 12 16
