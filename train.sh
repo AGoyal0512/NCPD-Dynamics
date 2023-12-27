e33e63ea99ee0d3be28580568d7eda11f0066094
Agam Goyal <goyalagam0@gmail.com>
2023-12-26 23:11:54 -0600

    results for k=10 and k=25

--- train.sh
+++ train.sh
@@ -1,16 +1,3 @@
-python3 scripts/oscillator_datagen.py -m FCA -n NWS -k 10
-python3 scripts/oscillator_datagen.py -m FCA -n ER -k 10 
-python3 scripts/oscillator_datagen.py -m FCA -n BA -k 10 
-python3 scripts/oscillator_datagen.py -m FCA -n NWS -k 25 
-python3 scripts/oscillator_datagen.py -m FCA -n ER -k 25 
-python3 scripts/oscillator_datagen.py -m FCA -n BA -k 25 
-python3 scripts/oscillator_datagen.py -m FCA -n NWS -k 35 
-python3 scripts/oscillator_datagen.py -m FCA -n ER -k 35 
-python3 scripts/oscillator_datagen.py -m FCA -n BA -k 35 
-python3 scripts/oscillator_datagen.py -m FCA -n NWS -k 50
-python3 scripts/oscillator_datagen.py -m FCA -n ER -k 50 
-python3 scripts/oscillator_datagen.py -m FCA -n BA -k 50 
-
 python3 main.py -m FCA -n NWS -k 10 -r 4 8 12 16
 python3 main.py -m FCA -n ER -k 10 -r 4 8 12 16
 python3 main.py -m FCA -n BA -k 10 -r 4 8 12 16
@@ -20,6 +7,3 @@ python3 main.py -m FCA -n BA -k 25 -r 4 8 12 16
 python3 main.py -m FCA -n NWS -k 35 -r 4 8 12 16
 python3 main.py -m FCA -n ER -k 35 -r 4 8 12 16
 python3 main.py -m FCA -n BA -k 35 -r 4 8 12 16
-python3 main.py -m FCA -n NWS -k 50 -r 4 8 12 16
-python3 main.py -m FCA -n ER -k 50 -r 4 8 12 16
-python3 main.py -m FCA -n BA -k 50 -r 4 8 12 16
